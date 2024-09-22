d0530027c47209858949c18e7014d1e5b8c0c2e5
zhoulq <615741631@qq.com>
2024-09-22 20:19:53 +0900

    添加编译脚本

--- document/build/apk/build.sh
+++ document/build/apk/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
  flutter clean
 
+#得到当前用户目录
+ userRoot=$(echo ~)
+
 # 在打包apk之前,请先修改一个配置
 # /Users/zhoulq/.pub-cache/hosted/pub.dev/image_gallery_saver-2.0.3/android/build.gradle
 # 将上面这个文件中的compileSdkVersion 30改为compileSdkVersion 31
 # 因为这个依赖使用了旧版本的SDK编译,更新版本之后导致报错,后续image_gallery_saver依赖版本跟新之后可能会解决这个问题
 # 暂时先强制修改依赖中的SDK版本号来解决这个问题
-file="~/.pub-cache/hosted/pub.dev/image_gallery_saver-2.0.3/android/build.gradle"
+file=$userRoot"/.pub-cache/hosted/pub.dev/image_gallery_saver-2.0.3/android/build.gradle"
 
 # 使用 sed 替换字符串并写回文件
 # linux环境请执行sed -i 's/compileSdkVersion 30/compileSdkVersion 31/g' $file
--- document/build/build-platform.sh
+++ document/build/build-platform.sh
@@ -3,14 +3,14 @@
 
 projectRoot=$(pwd)/../..
 
+##打包dmg
+#./mac/build.sh $projectRoot
+#mv $projectRoot/build/macos/Build/Products/Release/DairoDFS.dmg DairoDFS.dmg
+#
+##打包IOS
+#./ios/build.sh  $projectRoot
+#mv $projectRoot/build/ios/iphoneos/DairoDFS.ipa DairoDFS.ipa
+
 #打包APK
 ./apk/build.sh
 mv $projectRoot/build/app/outputs/flutter-apk/app-release.apk DairoDFS.apk
-
-#打包dmg
-./mac/build.sh $projectRoot
-mv $projectRoot/build/macos/Build/Products/Release/DairoDFS.dmg DairoDFS.dmg
-
-#打包IOS
-./ios/build.sh  $projectRoot
-mv $projectRoot/build/ios/iphoneos/DairoDFS.ipa DairoDFS.ipa
