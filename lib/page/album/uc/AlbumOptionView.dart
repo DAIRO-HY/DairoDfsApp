23c46e476ce5d3a8f88dce76d4641d5c569841d0
zhoulq <1064348912@qq.com>
2025-03-05 11:04:58 +0900

    相册功能添加上传功能

--- lib/page/album/uc/AlbumOptionView.dart
+++ lib/page/album/uc/AlbumOptionView.dart
@@ -1,10 +1,20 @@
+import 'dart:io';
+
+import 'package:dairo_dfs_app/extension/String++.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:dairo_dfs_app/api/FilesApi.dart';
 import 'package:dairo_dfs_app/extension/BuildContext++.dart';
 import 'package:dairo_dfs_app/extension/ValueNotifier++.dart';
+import '../../../db/dao/UploadDao.dart';
+import '../../../db/dto/UploadDto.dart';
 import '../../../uc/UCOptionMenuButton.dart';
 import '../../../uc/dialog/UCAlertDialog.dart';
+import '../../../util/WaitDialog.dart';
+import '../../../util/shared_preferences/SettingShared.dart';
+import '../../../util/upload/UploadTask.dart';
 import '../../home/HomePage.dart';
+import '../../transfer/TransferPage.dart';
 import '../AlbumPage.dart';
 
 ///操作菜单自定义组件
@@ -46,7 +56,7 @@ class AlbumOptionView extends StatelessWidget {
     //当前被选中的文件数量
     int selectedCount = this.albumPageState.selectedCount;
     this.optionMenu = [
-      // UCOptionMenuButton("全选", icon: Icons.library_add_check_outlined, onPressed: this.onCheckAllClick),
+      UCOptionMenuButton("上传", icon: Icons.add, onPressed: this.onAddClick),
       UCOptionMenuButton("删除", icon: Icons.delete_forever_outlined, disabled: selectedCount == 0, onPressed: this.onDeleteClick),
       UCOptionMenuButton("下载", icon: Icons.download_for_offline_outlined, disabled: selectedCount == 0, onPressed: this.onDownloadClick),
       UCOptionMenuButton("刷新", icon: Icons.refresh_outlined, onPressed: this.albumPageState.albumGrid.reload),
@@ -74,6 +84,45 @@ class AlbumOptionView extends StatelessWidget {
   ///分享按钮点击事件
   void onShareClick(){
     // UCShare.show(this._context, this.albumPageState.ucAlbumListView.selectedPaths);
+  }
+
+  ///上传按钮点击事件
+  void onAddClick() async {
+    Navigator.of(this._context).pop();
+
+    //文件选择时可能需要从ICloud下载,需要花费时间,所以这里最好显示等待框
+    WaitDialog.show(this._context);
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      //压缩质量越低，文件大小越小，但图像质量也会随之降低。0-100。
+      //compressionQuality: 1,
+
+      //指定只选择文件类型。
+      type: FileType.media,
+
+      //自定义文件选择对话框的标题。
+      dialogTitle: "选择上传文件",
+
+      //锁定父窗口，使用户在文件选择对话框打开时无法与其他窗口内容进行交互。
+      lockParentWindow: true,
+
+      //允许多选
+      allowMultiple: true,
+    );
+    WaitDialog.hide(this._context);
+    if (result != null) {
+      //获取最后一次打开的文件夹
+      const uploadFolder = "/相册";
+      final uploadDtoList = result.paths.map((it) {
+        final dto = UploadDto(name: it!.fileName, size: File(it).lengthSync(), path: it, dfsFolder: uploadFolder);
+        return dto;
+      }).toList();
+
+      //添加到数据库
+      UploadDao.insert(uploadDtoList);
+      UploadTask.start();
+
+      this._context.toPage(TransferPage(pageTag: TransferPage.PAGE_UPLOAD));
+    }
   }
 
   ///退出按钮点击事件
